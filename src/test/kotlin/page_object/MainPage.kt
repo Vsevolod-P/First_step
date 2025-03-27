c1ad335a4953d8c76204dd40a76b320b5377a38a
Vsevolod-P <discaple@gmail.com>
2025-03-27 16:23:24 +0300

    перенес локаторы в переменные и в одно место

--- src/test/kotlin/page_object/MainPage.kt
+++ src/test/kotlin/page_object/MainPage.kt
@@ -9,48 +9,49 @@ import kotlin.test.assertTrue
 class MainPage(driver: WebDriver) : BasePage(driver)  {
 
     private val focusedMenuTab = "tab_tab_type_current__2BEPc"
-    private val bunTab = "//span[text()='Булки']/.."
-    private val soucesTab = "//span[text()='Соусы']/.."
-    private val ingridientTab = "//span[text()='Начинки']/.."
+    private val bunTab = By.xpath("//span[text()='Булки']/..")
+    private val soucesTab = By.xpath("//span[text()='Соусы']/..")
+    private val ingridientTab = By.xpath("//span[text()='Начинки']/..")
+    private val waitBurgetText = By.xpath("//*[contains(text(),'Соберите бургер')]")
 
     @Step("Ждем загрузки главной")
     fun waitForMainPage() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Соберите бургер')]")))
+        wait.until(ExpectedConditions.visibilityOfElementLocated(waitBurgetText))
     }
 
     @Step("переходим на булки")
     fun goToBun() {
-        driver.findElement(By.xpath(bunTab)).click()
+        driver.findElement(bunTab).click()
     }
 
     @Step("переходим на соусы")
     fun goToSouces() {
-        driver.findElement(By.xpath(soucesTab)).click()
+        driver.findElement(soucesTab).click()
     }
 
     @Step("переходим на начинки")
     fun goToIngridient() {
-        driver.findElement(By.xpath(ingridientTab)).click()
+        driver.findElement(ingridientTab).click()
     }
 
 
     @Step("ждем загрузки главной страницы переключенной на меню булок и проверяем локатор переключения")
     fun TextWaitForMainPageSelectBunsAndCheckSelectTab(){
-        val tabElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(bunTab)))
+        val tabElement = wait.until(ExpectedConditions.visibilityOfElementLocated(bunTab))
         val currentClassName = tabElement.getAttribute("class")
         assertTrue(currentClassName.contains(focusedMenuTab), "Не найден локатор")
     }
 
     @Step("ждем загрузки главной страницы переключенной на меню суосов и проверяем локатор переключения")
     fun waitForMainPageSoucesAndCheckSelectTab() {
-        val tabElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(soucesTab)))
+        val tabElement = wait.until(ExpectedConditions.visibilityOfElementLocated(soucesTab))
         val currentClassName = tabElement.getAttribute("class")
         assertTrue(currentClassName.contains(focusedMenuTab), "Не найден локатор")
     }
 
     @Step("ждем загрузки главной страницы переключенной на меню начинок и проверяем локатор переключения")
     fun waitForMainPageIngridientAndCheckSelectTab() {
-        val tabElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(ingridientTab)))
+        val tabElement = wait.until(ExpectedConditions.visibilityOfElementLocated(ingridientTab))
         val currentClassName = tabElement.getAttribute("class")
         assertTrue(currentClassName.contains(focusedMenuTab), "Не найден локатор")
     }
